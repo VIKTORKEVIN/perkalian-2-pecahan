1d86587266c4f40879399ad1366ec56580de5331
VIKTORKEVIN <46395918+VIKTORKEVIN@users.noreply.github.com>
2019-01-05 19:04:34 +0700

    Add files via upload

---
+++ program perkalian 2 pecahan.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float hasil;
+
+    cout<<"==PROGRAM PERKALIAN 2 PECAHAN=="<<endl;
+    cout<<"p1=3.0 / 4.0"<<endl;
+    cout<<"p2=2.0 / 3.0"<<endl;
+
+    hasil=(3.0*2.0)/(3.0*4.0);
+
+    cout<<"Hasilnya adalah="<<hasil;
+    return 0;
+}
